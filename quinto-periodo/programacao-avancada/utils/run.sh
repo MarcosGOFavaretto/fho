5309c08cea52d00d553181a4a50731562b6a6cc6
Marcos Gabriel de Oliveira Favaretto <marcosfavaretto.dev@gmail.com>
2025-02-21 23:23:16 +0000

    :rocket:FEATURE: Creates a run script to execute C apps

---
+++ quinto-periodo/programacao-avancada/utils/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Informe o arquivo que deve ser construído!"
+    exit 1
+fi
+
+FILE=$1
+OUTPUT="${FILE%.c}.out"
+
+gcc -o "$OUTPUT" "$FILE"
+
+if [ $? -eq 0 ]; then
+    ./$OUTPUT
+else
+    echo "Erro na compilação."
+fi

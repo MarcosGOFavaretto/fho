409cdc36e9600d918392be738504ca073e9ab3a7
Marcos Gabriel de Oliveira Favaretto <marcosfavaretto.dev@gmail.com>
2026-02-24 01:24:10 +0000

    :rocket:Saving 23/02/2026 work

---
+++ setimo-periodo/analise-e-projetos-orientados-a-objetos-i/aula1/MB/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        string nome1, nome2, nome3;
+        int quantidade1, quantidade2, quantidade3;
+
+        Console.WriteLine("Nome do Produto 1:");
+        nome1 = Console.ReadLine();
+        Console.WriteLine("Quantidade em estoque: ");
+        quantidade1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Nome do Produto 2:");
+        nome2 = Console.ReadLine();
+        Console.WriteLine("Quantidade em estoque: ");
+        quantidade2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Nome do Produto 3:");
+        nome3 = Console.ReadLine();
+        Console.WriteLine("Quantidade em estoque: ");
+        quantidade3 = int.Parse(Console.ReadLine());
+
+        string produtoMenorEstoque = nome1;
+        int menorEstoque = quantidade1;
+
+        if (quantidade2 < menorEstoque)
+        {
+            produtoMenorEstoque = nome2;
+            menorEstoque = quantidade2;
+        }
+
+        if (quantidade3 < menorEstoque)
+        {
+            produtoMenorEstoque = nome3;
+            menorEstoque = quantidade3;
+        }
+
+        Console.WriteLine($"\nProduto com menor estoque: {produtoMenorEstoque}");
+        Console.WriteLine($"Quantidade: {menorEstoque}");
+    }
+}
